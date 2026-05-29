fd3e7949510b17ebc214c2d8b0b3d825012b4c24
Igor Bruno <igorbrunojucurutu@gmail.com>
2026-05-29 16:44:59 -0300

    Mudanças na interface do sistema

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ controle = True
 def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+# Menu principal do sistema
 while controle == True:
     limpar_tela()
-    # Menu principal do sistema
     print('''
     =========================================
-          SISTEMA DE GESTÃO - PYSHOW
+          SISTEMA DE GESTÃO - ROCK BASEMENT
     =========================================
     1. Modulo Artistas
     2. Modulo Shows
@@ -23,6 +23,8 @@ while controle == True:
 
     # Modulo de Artistas
     if opcao_menu == 1:
+        print("Carregando...")
+        sleep(1)
         while  True:
             limpar_tela()
             print('''
@@ -42,23 +44,30 @@ while controle == True:
                 nome = input("Nome do artista: ")
                 cache = input("Cache do artista: ")
                 genero_musical = input("Genero músical do artista: ")
+
             elif opcao_artistas == 2:
                 print("LISTAR ARTISTAS")
                 print("⚠️Estamos desenvolvendo isso")
+
             elif opcao_artistas == 3:
                 print("EDITAR ARTISTAS")
+
             elif opcao_artistas == 4:
                 print("Excluir ARTISTAS")
                 print("⚠️Estamos desenvolvendo isso")
+
             elif opcao_artistas == 0:
                 print("Saindo do modulo...")
                 sleep(1)
                 break
+
             else:
-                print("Opção invalida, tente novamente")
+                print("✋👺🚫Opção invalida, tente novamente")
 
     # Modulo de shows
     elif opcao_menu == 2:
+        print("Carregando...")
+        sleep(1)
         while True:
             limpar_tela()
             print('''
@@ -73,28 +82,37 @@ while controle == True:
     =========================================
     ''')
             opcao_show = int(input("Escolha uma opção: "))
+        
             if opcao_show == 1:
                 print("ADICIONAR SHOW")
                 nome_show = input("Nome do show: ")
                 artistas_show = input("Nome dos artistas: ")
                 genero_show = input("Estilos músicais: ")
+
             elif opcao_show == 2:
                 print("LISTAR SHOWS")
                 print("⚠️Estamos desenvolvendo isso")
+
             elif opcao_show == 3:
                 print("EDITAR SHOWS")
+
             elif opcao_show == 4:
                 print("Excluir SHOWS")
                 print("⚠️Estamos desenvolvendo isso")
+
             elif opcao_show == 0:
                 print("Saindo do modulo...")
                 sleep(1)
                 break
+
             else:
-                print("Opção invalida, tente novamente")
+                print("✋👺🚫Opção invalida, tente novamente")
+
     
     # Modulo de shows
     elif opcao_menu == 3:
+        print("Carregando...")
+        sleep(1)
         while True:
             limpar_tela()
             print('''
@@ -105,39 +123,51 @@ while controle == True:
     2. Listar Ingressos
     3. Editar Ingressos
     4. Deletar Ingressos
+    5. Registrar Vendas
+    6. Listar Vendas
     0. Sair do Modulo
     =========================================
     ''')
             opcao_bilheteria = int(input("Escolha uma opção: "))
             if opcao_bilheteria == 1:
-                print("ADICIONAR SHOW")
+                print("ADICIONAR INGRESSO")
                 ingresso_show =  input("Para qual show é o ingresso? ")
                 ingresso_quant = input("Número de ingressos? ")
+                ingresso_tipo = input("Qual o tipo de ingresso? ")
+                ingresso_preco = input("Preco do ingresso")
+
             elif opcao_bilheteria == 2:
-                print("LISTAR SHOWS")
+                print("LISTAGEM DE INGRESSOS")
                 print("⚠️Estamos desenvolvendo isso")
+
             elif opcao_bilheteria == 3:
-                print("EDITAR SHOWS")
+                print("EDITAR INGRESSOS")
+
             elif opcao_bilheteria == 4:
-                print("Excluir SHOWS")
+                print("EXCLUIR INGRESSOS")
                 print("⚠️Estamos desenvolvendo isso")
+
+            elif opcao_bilheteria == 5:
+                print("REGISTRO DE VENDAS")
+                print("⚠️Estamos desenvolvendo isso")
+
+            elif opcao_bilheteria == 6:
+                print("Lista de Vendas")
+                print("⚠️Estamos desenvolvendo isso")
+
+
             elif opcao_bilheteria == 0:
                 print("Saindo do modulo...")
                 sleep(1)
                 break
+
             else:
-                print("Opção invalida, tente novamente")
+                print("✋👺🚫Opção invalida, tente novamente")
     
     elif opcao_menu == 0:
         print("Saindo do programa")
         sleep(1)
         controle = False
 
-        
-
-
-
-        
-
-
-
+    else:
+        print("✋👺🚫Opção invalida, tente novamente")
